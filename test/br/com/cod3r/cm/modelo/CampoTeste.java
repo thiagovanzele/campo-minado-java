27b0055c5be74a2a478cfeba8aec15648c7fa2c8
Thiago Vanzele <thiagovanzele@gmail.com>
2023-07-03 15:09:23 -0300

    Implementando metodo adicionar vizinho

---
+++ test/br/com/cod3r/cm/modelo/CampoTeste.java
@@ -0,0 +1,81 @@
+package br.com.cod3r.cm.modelo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CampoTeste {
+
+	private Campo campo;
+
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3, 3);
+	}
+
+	@Test
+	void testeVizinhoRealDistancia1Esquerda() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoRealDistancia1Direita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoRealDistancia1Cima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoRealDistancia1Baixo() {
+		Campo vizinho = new Campo(4, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoDistancia2DiagonalCimaEsquerda() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia2DiagonalCimaDireita() {
+		Campo vizinho = new Campo(2, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia2DiagonalBaixoEsquerda() {
+		Campo vizinho = new Campo(4, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia2DiagonalBaixoDireita() {
+		Campo vizinho = new Campo(4, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(1, 1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+
+}
